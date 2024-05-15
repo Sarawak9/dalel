7d4686bb43602e63d7e61d69c867fada3273091a
Sarawak9 <muhammadesraa561@gmail.com>
2024-05-15 20:36:04 +0300

    App Colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dalel/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,11 @@ class Dalel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        home: Scaffold(
+      body: Container(
+        child: Image.asset(Assets.imagesOnBoarding1),
+      ),
+    ));
   }
 }
-
