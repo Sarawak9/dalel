cee9a67cc50a41ee885183adfbd57e567a104223
Sarawak9 <muhammadesraa561@gmail.com>
2024-05-15 09:06:36 +0300

    done

---
+++ lib/const/resource.dart
@@ -0,0 +1,30 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class R {
+  const R._();
+
+  /// ![preview](file://E:\flutter_projects\dalel\assets\images\Success.png)
+  static const String ASSETS_IMAGES_SUCCESS_PNG = 'assets/images/Success.png';
+
+  /// ![preview](file://E:\flutter_projects\dalel\assets\images\Vector1.svg)
+  static const String ASSETS_IMAGES_VECTOR1_SVG = 'assets/images/Vector1.svg';
+
+  /// ![preview](file://E:\flutter_projects\dalel\assets\images\Vector2.svg)
+  static const String ASSETS_IMAGES_VECTOR2_SVG = 'assets/images/Vector2.svg';
+
+  /// ![preview](file://E:\flutter_projects\dalel\assets\images\onBoarding1.png)
+  static const String ASSETS_IMAGES_ONBOARDING1_PNG =
+      'assets/images/onBoarding1.png';
+
+  /// ![preview](file://E:\flutter_projects\dalel\assets\images\onBoarding2.png)
+  static const String ASSETS_IMAGES_ONBOARDING2_PNG =
+      'assets/images/onBoarding2.png';
+
+  /// ![preview](file://E:\flutter_projects\dalel\assets\images\onBoarding3.png)
+  static const String ASSETS_IMAGES_ONBOARDING3_PNG =
+      'assets/images/onBoarding3.png';
+
+  /// ![preview](file://E:\flutter_projects\dalel\assets\images\vector.svg)
+  static const String ASSETS_IMAGES_VECTOR_SVG = 'assets/images/vector.svg';
+}
---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,32 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesOnBoarding1
+  /// assets/images/onBoarding1.png
+  static const String imagesOnBoarding1 = "assets/images/onBoarding1.png";
+
+  /// Assets for imagesOnBoarding2
+  /// assets/images/onBoarding2.png
+  static const String imagesOnBoarding2 = "assets/images/onBoarding2.png";
+
+  /// Assets for imagesOnBoarding3
+  /// assets/images/onBoarding3.png
+  static const String imagesOnBoarding3 = "assets/images/onBoarding3.png";
+
+  /// Assets for imagesSuccess
+  /// assets/images/Success.png
+  static const String imagesSuccess = "assets/images/Success.png";
+
+  /// Assets for imagesVector
+  /// assets/images/vector.svg
+  static const String imagesVector = "assets/images/vector.svg";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector1.svg
+  static const String imagesVector1 = "assets/images/Vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/Vector2.svg
+  static const String imagesVector2 = "assets/images/Vector2.svg";
+}
+
